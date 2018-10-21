e8dae74fc53fddc483e884fc107b842c40a7f99c
Simon Painter <simon@Simons-MacBook.home>
2018-10-21 19:45:28 +0100

    Reformatted some more.

--- pyflare.py
+++ pyflare.py
@@ -3,25 +3,15 @@ import json
 
 import requests
 
-with open('config.json') as json_data_file:
-    config = json.load(json_data_file)
-
-email = config['email']
-key = config['key']
-zone = config['zone']
-record = config['record']
-
-
-def ipify():
-    r = requests.get("https://api.ipify.org/")
-    r.raise_for_status()
-    return r.text
-
-
 class Cloudflare:
     def __init__(self, email, key):
         self.endpoint = "https://api.cloudflare.com/client/v4"
         self.headers = {'X-Auth-Email': email, 'X-Auth-Key': key, 'Content-Type': 'application/json'}
+
+    def getmyip(self):
+        r = requests.get("https://api.ipify.org/")
+        r.raise_for_status()
+        return r.text
 
     def user(self):
         r = requests.get(self.endpoint + "/user", headers=self.headers)
@@ -42,11 +32,21 @@ class Cloudflare:
         r = requests.put(self.endpoint + "/zones/" + zone_id + "/dns_records/" + record_id, headers=self.headers, data=json.dumps(payload))
         return r.json()
 
+    def __call__(self,zone,record):
+        zone_id = cf.zones(zone)['result'][0]['id']
+        record_id = cf.dns_records(zone_id, record)['result'][0]['id']
+        ip_address = cf.getmyip()
+        if ip_address != cf.dns_records(zone_id, record)['result'][0]['content']:
+            return cf.update_record(zone_id, record_id, record, ip_address)
+        else:
+            return "OK"
 
-cf = Cloudflare(email, key)
-zone_id = cf.zones(zone)['result'][0]['id']
-record_id = cf.dns_records(zone_id, record)['result'][0]['id']
-
-ip_address = ipify()
-
-print(cf.update_record(zone_id, record_id, record, ip_address))
+if __name__ == '__main__':
+    with open('config.json') as json_data_file:
+        config = json.load(json_data_file)
+    email = config['email']
+    key = config['key']
+    zone = config['zone']
+    record = config['record']
+    cf = Cloudflare(email, key)
+    print(cf(zone,record))
